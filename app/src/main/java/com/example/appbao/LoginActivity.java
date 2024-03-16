08e6154d8b91f4359b52e40eb653414a5cba0241
ADMIN <minhphuc9401@gmail.com>
2024-03-16 14:37:20 +0700

    Phuc update

--- app/src/main/java/com/example/appbao/LoginActivity.java
+++ app/src/main/java/com/example/appbao/LoginActivity.java
@@ -27,7 +27,7 @@ public class LoginActivity extends AppCompatActivity {
     TextView mCreateBtn;
     ProgressBar progressBar;
     FirebaseAuth fAuth;
-//Testttttssssssss
+    //Chieu nay di lam nha hang
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
