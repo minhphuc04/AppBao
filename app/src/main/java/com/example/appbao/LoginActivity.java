4d966e2502bdf99ae15f6479b348bdba7b59efe0
DuyGTX <quangduy1997vn@gmail.com>
2024-03-24 01:45:39 +0700

    Duy vừa siêu update

--- app/src/main/java/com/example/appbao/LoginActivity.java
+++ app/src/main/java/com/example/appbao/LoginActivity.java
@@ -5,10 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -26,6 +28,7 @@ public class LoginActivity extends AppCompatActivity {
     ProgressBar loginProgress;
     TextView createText;
     FirebaseAuth mAuth;
+    private boolean passwordShowing=false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,8 +40,26 @@ public class LoginActivity extends AppCompatActivity {
         loginProgress = findViewById(R.id.progressBar);
         btnLogin = findViewById(R.id.loginbtn);
         createText = findViewById(R.id.createtext);
-
+        final ImageView passwordIc=findViewById(R.id.ic_password);
         mAuth = FirebaseAuth.getInstance();
+
+        passwordIc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(passwordShowing){
+                    passwordShowing=false;
+                    userPassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                    passwordIc.setImageResource(R.drawable.ic_show_password);
+                }
+                else {
+                    passwordShowing=true;
+                    userPassword.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                    passwordIc.setImageResource(R.drawable.ic_hide_password);
+                }
+                userPassword.setSelection(userPassword.length());
+            }
+        });
 
         createText.setOnClickListener(new View.OnClickListener() {
             @Override
